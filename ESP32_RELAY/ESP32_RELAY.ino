eb42c569582c95458ec07d0f9b493be8e112d045
INOVATRIX <143087722+INOVATRIX@users.noreply.github.com>
2023-08-29 10:16:51 +0530

    Add files via upload

---
+++ ESP32_RELAY/ESP32_RELAY.ino
@@ -0,0 +1,16 @@
+#define relay1 5                  //  Define Pin of the Relay
+
+void setup(){
+
+  pinMode(relay1,OUTPUT);         // Setting the Pin to output signal
+
+}
+
+void loop(){
+
+  digitalWrite(relay1, LOW);    // Writing value "LOW" to the pin
+  delay(1000);                  // Delay in miliseconds 
+  digitalWrite(relay1, HIGH);   // Writing value "HIGH" to the pin
+  delay(1000);
+
+}
